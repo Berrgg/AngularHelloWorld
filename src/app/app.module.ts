1eda72e25f1ba882425fe867f5f39f644479d694
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2020-04-18 09:32:00 +0100

    Project init

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCaseWordPipe } from './title-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FavoriteComponent,
+    TitleCaseWordPipe
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent {
+    @Input() isFavorite: boolean;
+    values = '';
+    title: string;
+
+    onClick(){
+        this.isFavorite = !this.isFavorite;
+    }
+
+    onKey(event: any){
+        this.values = event.target.value;
+    }
+}
